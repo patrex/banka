c50317cc66d2eb6714664d6732ae936c21a358b0
patsoks <patsoks.sokari@gmail.com>
2019-04-09 13:41:31 +0100

    feat(template): add feature for users to view exchange rates
    [Delivers #165213196]

--- UI/js/landing.js
+++ UI/js/landing.js
@@ -12,3 +12,27 @@ closeBtn.addEventListener('click', (e) => {
     list.classList.toggle('active');
     menuBar.style.display = '';
 });
+
+const nairaDollar = document.querySelector('#nairaDollar');
+const nairaGBP = document.querySelector('#nairaGBP');
+const nairaJPY = document.querySelector('#nairaJPY');
+
+const showCurr = () => {
+    const refTabs = {
+        ngnGBP: 0.00211,
+        ngnJPY: .31,
+    }
+    const url = 'https://openexchangerates.org/api/latest.json?app_id=b3cc319547964c44bd16f451895333f8&base=USD&symbols=NGN,JPY,GBP';
+    fetch(url)
+        .then((results) => results.json())
+        .then(data => {
+            nairaDollar.innerHTML = (data.rates.NGN).toFixed(2);
+            nairaGBP.innerHTML = (1 / refTabs.ngnGBP).toFixed(2);
+            nairaJPY.innerHTML = (1 / refTabs.ngnJPY).toFixed(2);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+} 
+
+setInterval(showCurr, 60000);    //attempt to update currencies every minute
