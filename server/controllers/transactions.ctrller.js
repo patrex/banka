80158a1ce8c3ab73427d3a952f88c70c8cb78a6b
patsoks <patsoks.sokari@gmail.com>
2019-04-12 19:26:48 +0100

    chore(app): refactor endpoint callbacks to controllers

---
+++ server/controllers/transactions.ctrller.js
@@ -0,0 +1,90 @@
+/* eslint-disable class-methods-use-this */
+import users from '../models/users';
+import accounts from '../models/accounts';
+import transactions from '../models/transactions';
+
+export default class {
+  debitAcct(req, res) {
+    const { accountNumber } = req.params;
+    const { amount, cashierID, transType } = req.body;
+    const acctID = accounts.findIndex(account => account.acctNumber === accountNumber);
+    const userID = users.findIndex(user => user.userID === cashierID);
+    if (acctID !== -1) {
+      if (userID !== -1) {
+        if (users[userID].userType === 'cashier') {
+          const transID = transactions.length;
+          const transaction = {
+            transID,
+            amount,
+            acctNumber: accounts[acctID].acctNumber,
+            cashierID,
+            transType,
+            acctBal: accounts[acctID].openingBalance - amount,
+          };
+          transactions.push(transaction);
+          res.status(201).json({
+            status: 201,
+            data: transactions[transID],
+          });
+        } else {
+          res.status(403).json({
+            status: 403,
+            message: 'Forbidden',
+          });
+        }
+      } else {
+        res.status(400).json({
+          status: 400,
+          message: 'This username not found',
+        });
+      }
+    } else {
+      res.status(400).json({
+        status: 400,
+        message: 'This account not found',
+      });
+    }
+  }
+
+  creditAcct(req, res) {
+    const { accountNumber } = req.params;
+    const { amount, cashierID, transType } = req.body;
+    const acctID = accounts.findIndex(account => account.acctNumber === accountNumber);
+    const userID = users.findIndex(user => user.userID === cashierID);
+    if (acctID !== -1) {
+      if (userID !== -1) {
+        if (users[userID].userType === 'cashier') {
+          const transID = transactions.length;
+          const transaction = {
+            transID,
+            amount,
+            acctNumber: accounts[acctID].acctNumber,
+            cashierID,
+            transType,
+            acctBal: accounts[acctID].openingBalance + amount,
+          };
+          transactions.push(transaction);
+          res.status(201).json({
+            status: 201,
+            data: transactions[transID],
+          });
+        } else {
+          res.status(403).json({
+            status: 403,
+            message: 'Forbidden',
+          });
+        }
+      } else {
+        res.status(400).json({
+          status: 400,
+          message: 'This username not found',
+        });
+      }
+    } else {
+      res.status(400).json({
+        status: 400,
+        message: 'This account not found',
+      });
+    }
+  }
+}
---
+++ server/controllers/users.ctrller.js
@@ -0,0 +1,88 @@
+/* eslint-disable class-methods-use-this */
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import users from '../models/users';
+
+export default class {
+  sayHi(req, res) {
+    res.status(200).json({
+      status: 200,
+      message: 'Hello, there...',
+    });
+  }
+
+  createUser(req, res) {
+    const userID = users.length;
+    const username = req.body.fname.toLowerCase() + userID;
+    const saltRounds = 10;
+    const user = {
+      userID,
+      firstname: req.body.fname,
+      midname: req.body.mname,
+      lname: req.body.lname,
+      username,
+      email: req.body.email,
+      userType: req.body.type, // normal, cashier, admin
+    };
+    bcrypt.hash(req.body.pwd, saltRounds).then((hash) => {
+      user.password = hash;
+    }).then(() => {
+      bcrypt.hash(req.body.rpwd, saltRounds).then((hash) => {
+        user.rpassword = hash;
+      }).then(() => {
+        const token = jwt.sign(user, 's3cr3t');
+        user.token = token;
+        users.push(user);
+      }).then(() => {
+        res.status(200).json({
+          status: 200,
+          data: {
+            token: users[userID].token,
+            firstname: users[userID].firstname,
+            lastname: users[userID].lastname,
+            username: users[userID].username,
+            email: users[userID].email,
+            userType: users[userID].userType,
+          },
+        });
+      })
+        .catch(() => {
+          res.status(500).json({
+            status: 500,
+            data: {
+              status: 500,
+              message: 'The account could not be created at this time',
+            },
+          });
+        });
+    });
+  }
+
+  signIn(req, res) {
+    const { uname, pwd } = req.body;
+    const userID = users.findIndex(user => user.username === uname);
+    if (userID >= 0) {
+      bcrypt.compare(pwd, users[userID].password).then((rez) => {
+        if (rez === true) {
+          return res.status(200).json({
+            status: 200,
+            data: {
+              token: users[userID].token,
+              firstname: users[userID].firstname,
+              lastname: users[userID].lastname,
+              username: users[userID].username,
+              email: users[userID].email,
+            },
+          });
+        }
+        res.status(500).json({
+          status: 500,
+          data: {
+            status: 500,
+            message: 'Sorry. We could not verify the information you provided',
+          },
+        });
+      });
+    } else res.json({ message: 'UserID not found!' });
+  }
+}
