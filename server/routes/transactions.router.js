c525b1b84616b31c9c00082cbbaac3f93310c89d
patsoks <patsoks.sokari@gmail.com>
2019-04-12 20:18:04 +0100

    feat(app): add feature for admin/staff to view all transactions
    [Delivers #165146091]

--- server/routes/transactions.router.js
+++ server/routes/transactions.router.js
@@ -4,6 +4,7 @@ import TransCtrller from '../controllers/transactions.ctrller';
 const transCtrller = new TransCtrller();
 const router = express.Router({ mergeParams: true });
 
+router.get('/', transCtrller.getAll);
 router.post('/:accountNumber(\\d+)/debit', transCtrller.debitAcct);
 router.post('/:accountNumber(\\d+)/credit', transCtrller.creditAcct);
 
