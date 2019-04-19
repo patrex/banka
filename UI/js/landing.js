edbb27407fdfca1b12b015e2d8a08bc0ff33c3b7
Patsoks <patsoks.sokari@gmail.com>
2019-04-19 15:05:42 +0100

    feat(UI): add features
    - add feature for user to perform Kwik transacation
    - add password recovery option
    
    [Delivers #165434285]

--- UI/js/landing.js
+++ UI/js/landing.js
@@ -1,22 +1,27 @@
+/* eslint-disable linebreak-style */
 const menuBar = document.querySelector('#menu-btn');
 const list = document.querySelector('ul');
 const xClose = document.querySelector('.x-close');
 const optionBox = document.querySelector('#kwikTransactionType');
 const kwikSend = document.querySelector('.kwik-send');
 const kwikReceive = document.querySelector('.kwik-receive');
-const kwikSubmit = document.querySelector('#kwik-submit');
+const kwikSubmit = document.querySelector('#subBtn');
 
-menuBar.addEventListener('click', (e) => {
+const kwikTransactionId = document.querySelector('#kwik-transaction-id');
+const kwikTransactionAccountNumber = document.querySelector('#kwik-account-number');
+const kwikTransactionAmount = document.querySelector('#kwik-transaction-amount');
+
+menuBar.addEventListener('click', () => {
   list.classList.toggle('active');
   menuBar.style.display = 'none';
   xClose.style.display = 'inline';
 });
 
-xClose.addEventListener('click', (e) => {
+xClose.addEventListener('click', () => {
   list.classList.toggle('active');
   menuBar.style.display = '';
   xClose.style.display = '';
-})
+});
 
 const nairaDollar = document.querySelector('#nairaDollar');
 const nairaGBP = document.querySelector('#nairaGBP');
@@ -35,22 +40,22 @@ const showCurr = () => {
       nairaGBP.innerHTML = (1 / refTabs.ngnGBP).toFixed(2);
       nairaJPY.innerHTML = (1 / refTabs.ngnJPY).toFixed(2);
     })
-    .catch((err) => {
+    .catch(() => {
 
     });
-} 
+};
 
 // setInterval(showCurr, 60000);    // attempt to update currencies every minute
 
 const showKwikReceiveDialog = () => {
   kwikSend.style.display = 'none';
   kwikReceive.style.display = 'block';
-}
+};
 
 const showKwikSendDialog = () => {
   kwikReceive.style.display = 'none';
   kwikSend.style.display = 'block';
-}
+};
 
 optionBox.addEventListener('change', () => {
   if (optionBox.value === 'send') {
@@ -66,6 +71,37 @@ optionBox.addEventListener('change', () => {
 kwikSubmit.addEventListener('click', (e) => {
   if (optionBox.value === '') {
     e.preventDefault();
+    // eslint-disable-next-line no-alert
     alert('You did not choose any Kwik transaction option');
+    return;
+  }
+
+  if (optionBox.value === 'receive') {
+    if (kwikTransactionId.value.length !== 5) {
+      e.preventDefault();
+      // eslint-disable-next-line no-alert
+      alert('Invalid Transaction ID');
+      return;
+    }
+
+    // add api call here
+  }
+
+  if (optionBox.value === 'send') {
+    if (kwikTransactionAccountNumber.value.length < 1) {
+      e.preventDefault();
+      // eslint-disable-next-line no-alert
+      alert('Invalid Account Number');
+      return;
+    }
+  
+    if (kwikTransactionAmount.value <= 0) {
+      e.preventDefault();
+      // eslint-disable-next-line no-alert
+      alert('Invalid Transaction. Amount cannot be less than 1');
+      return;
+    }
+
+    // add api call here
   }
 });
