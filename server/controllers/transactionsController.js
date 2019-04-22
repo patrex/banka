ecb6d95f9a204a5bc6c86d1382726bb7331d48f1
Patsoks <patsoks.sokari@gmail.com>
2019-04-22 16:10:53 +0100

    feat(transactionController): add transactions view
    - use of SQL to fetch transactions
    - remove use of data structure for this purpose
    
    [Finishes #165432553]

--- server/controllers/transactionsController.js
+++ server/controllers/transactionsController.js
@@ -3,12 +3,25 @@ import users from '../models/usersModel';
 import accounts from '../models/accountsModel';
 import transactions from '../models/transactionsModel';
 
-export default class Transactions {
+export default class TransactionsController {
   getAll(req, res) {
-    return res.status(200).json({
-      status: 200,
-      data: transactions,
-    });
+    async getAllTransactions(req, res) {
+      const results = await transactionsModel.listAllTransactions();
+  
+      if (results.success) {
+        if (results.success.rowCount > 0) {
+          res.status(200).json({
+            status: 200,
+            data: results.success.rows,
+          });
+        } else {
+          res.status(404).json({
+            status: 404,
+            message: 'No results found. Check the account number and retry',
+          });
+        }
+      }
+    }
   }
 
   debitAcct(req, res) {
