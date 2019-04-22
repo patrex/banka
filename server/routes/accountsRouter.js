16d371c5c04d6c131ab29c2c5ea409818ed89b0f
Patsoks <patsoks.sokari@gmail.com>
2019-04-22 14:39:32 +0100

    chore(accountRouter): rename function names and other identifiers
            [Finishes #165493662]

--- server/routes/accountsRouter.js
+++ server/routes/accountsRouter.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import AcctCtrller from '../controllers/accountsController';
-import AcctVal from '../middlewares/accountsValidator';
+import AccountController from '../controllers/accountsController';
+import AccountValidator from '../middlewares/accountsValidator';
 
-const acctVal = new AcctVal();
+const accountValidator = new AccountValidator();
+const accountController = new AccountController();
 
-const acctCtrller = new AcctCtrller();
 const router = express.Router({ mergeParams: true });
 
-router.get('/', acctCtrller.getAll);
-router.post('/', acctVal.valAcctInfo, acctCtrller.createAccount);
-router.patch('/:accountNumber(\\d+)', acctCtrller.activateDeactivate);
-router.delete('/:accountNumber(\\d+)', acctCtrller.deleteAcct);
+router.get('/', accountController.getAll);
+router.post('/', accountValidator.valAcctInfo, accountController.createAccount);
+router.patch('/:accountNumber(\\d+)', accountController.activateDeactivate);
+router.delete('/:accountNumber(\\d+)', accountController.deleteAcct);
 
 export default router;
